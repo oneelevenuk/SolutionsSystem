4cc197919419582a0902beceae0394959a2b6fa8
oneelevenuk <oneeleven@live.co.uk>
2015-02-03 09:45:26 +0000

    Create ext_dbcon.php

---
+++ phphinc/ext_dbcon.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$con=mysqli_connect("sql3.freemysqlhosting.net","sql365671","lV3*sD3*","sql365671");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+?>
